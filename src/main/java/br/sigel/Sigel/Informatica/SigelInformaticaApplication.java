9deecaffe21a67e4b54844c4a43a117ddf7e81f5
emaildoissa@gmail.com <emaildoissa@gmail.com>
2024-11-14 09:36:38 -0300

    Primeiro Commit

---
+++ src/main/java/br/sigel/Sigel/Informatica/SigelInformaticaApplication.java
@@ -0,0 +1,13 @@
+package br.sigel.Sigel.Informatica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigelInformaticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigelInformaticaApplication.class, args);
+	}
+
+}
