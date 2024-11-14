62519e7f84066cb486522f5e22fb23714ec5e234
emaildoissa@gmail.com <emaildoissa@gmail.com>
2024-11-14 09:45:57 -0300

    Primeiro Commit

---
+++ src/test/java/br/sigel/Sigel/Informatica/SigelInformaticaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.sigel.Sigel.Informatica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigelInformaticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
